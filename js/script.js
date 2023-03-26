d6b500cd6bff0c58b867660e3d8229be8e29ccdd
Lycan <lycanfsd@gmail.com>
2023-03-25 21:28:30 -0500

    Add event listener for click on grid

--- js/script.js
+++ js/script.js
@@ -7,7 +7,15 @@ const Gameboard = (() => {
     board[i] = 0;
   }
   const getBoard = () => board;
-  return { getBoard };
+  // Adds player mark to the board array. Position is from 0-8.
+  const addMark = (position, player) => {
+    if (board[position] === 0 && player.getMark() === "X") {
+      board[position] = 1;
+    } else if (board[position] === 0 && player.getMark() === "O") {
+      board[position] = 2;
+    }
+  };
+  return { getBoard, addMark };
 })();
 
 //Create Players
@@ -21,6 +29,12 @@ const createPlayer = (name, mark) => {
 const GameController = (() => {
   const playerOne = createPlayer("Player One", "X");
   const playerTwo = createPlayer("Player Two", "O");
+  let activePlayer = playerOne;
+  const switchTurns = () => {
+    activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+  };
+  const getActivePlayer = () => activePlayer;
+  return { getActivePlayer };
 })();
 
 //Display
@@ -32,4 +46,40 @@ const DisplayController = (() => {
     div.setAttribute("data-value", Gameboard.getBoard()[i]);
     main.appendChild(div);
   }
+  // Updates Display div to correct class based on board array value at that position
+  function updateDisplay() {
+    for (let i = 0; i < Gameboard.getBoard().length; i++) {
+      switch (Gameboard.getBoard()[i]) {
+        case 1: {
+          let div = document.querySelector(`#cell${i}`);
+          div.setAttribute("data-value", Gameboard.getBoard()[i]);
+          div.classList.add("is-marked-X");
+          break;
+        }
+        case 2: {
+          let div = document.querySelector(`#cell${i}`);
+          div.setAttribute("data-value", Gameboard.getBoard()[i]);
+          div.classList.add("is-marked-O");
+          break;
+        }
+        default:
+          break;
+      }
+    }
+  }
+  // Needs to take user click and see who clicked then add mark at specific clicked position
+  const divGrid = document.querySelectorAll(".cellGrid");
+  divGrid.forEach((div) => {
+    div.addEventListener("click", (e) => {
+      const arr = Array.prototype.slice.call(divGrid);
+      Gameboard.addMark(
+        arr.indexOf(e.target),
+        GameController.getActivePlayer()
+      );
+      updateDisplay();
+    });
+  });
 })();
+
+//Delete After
+console.log(Gameboard.getBoard());
